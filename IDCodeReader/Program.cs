4b8e87771f00efa45cb1b7282ceadbf9f8d67f4f
Opilane TTHK <opilane@tthk.ee>
2020-10-15 11:39:28 +0300

    commit

---
+++ IDCodeReader/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace IDCodeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your ID code");
+            string userID = Console.ReadLine();
+            if (Validate(userID))
+            {
+                Console.WriteLine("Welcome!");
+                HelloUser(userID);
+            }else
+            {
+                Console.WriteLine("Wrong format. Try again");
+            }
+        }
+        public static bool Validate(string idCode)
+        {
+            if(idCode.Length == 11)
+            {
+                try
+                {
+                    long.Parse(idCode);
+                    return true;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Wrong format: {e}");
+                    return false;
+                }
+                
+            } else
+            {
+                return false;
+            }
+            
+        }
+        public static void HelloUser(string idCode)
+        {
+            int firstNum = Int32.Parse(idCode[0].ToString());
+            if(firstNum ==1||firstNum == 3 || firstNum == 5)
+            {
+                Console.WriteLine("Hello Sir!");
+            } else if (firstNum == 2 || firstNum == 4 || firstNum == 6)
+            {
+                Console.WriteLine("Hello, Madam!");
+            }
+        }
+    }
+}
---
+++ LOGITpe20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LOGITpe20
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DisplayLOGIT("LOGITpe20");
+
+
+        }
+        public static void DisplayLOGIT(string logitpe20)
+        {
+            string logit = logitpe20.Substring(0, 5);
+            string pe = logitpe20.Substring(5, 2);
+            string twenty = logitpe20.Substring(7, 2);
+            Console.WriteLine($"--> {logit}");
+            Console.WriteLine($"--> {pe}");
+            Console.WriteLine($"--> {twenty}");
+        }
+
+    }
+}
